401e23d6f568d14a34d8ca559b8dad78108a3b13
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2019-04-07 06:45:31 -0400

    Create script.R

---
+++ script.R
@@ -0,0 +1,16 @@
+library(readxl)
+crimestats <- read_excel("http://katski.org/crimestats.xlsx")
+View(crimestats)
+
+library(ggplot)
+
+ggplot(crimestats, aes(x = density, y = crime, color = city))+
+    geom_point(show.legend = FALSE) + geom_text(aes(label=city),hjust=0, vjust=0) +
+    geom_line() +
+    ggplot2::theme(plot.title = ggplot2::element_text(face = "bold")) +
+    theme(legend.position = "none") +
+    ggplot2::labs(
+        x = "Population per Square Mile", y = "Crime Rate per 100,000 People",
+        color = NULL,
+        title = "Crime And Population Density in US Cities (1968)",
+        subtitle = "Data collected from https://www.census.gov/library/publications/1970/compendia/statab/91ed.html")
